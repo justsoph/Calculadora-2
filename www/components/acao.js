a9a39ee6245659a25d1536d22d8a60763da10039
justsoph <sophiaanaklumythic@gmail.com>
2021-04-02 06:36:35 +0900

    vai toma nco u

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
 
+var valor;
+var resultado;
+function button(num){
+  valor = document.calculadora.visor.value + num;
+}
+
+function reset(){
+  document.calculadora.visor.value = '';
+}
+
+function calcular(){
+  resultado = eval(valor);
+  document.calculadora.visor.value = resultado;
+}
