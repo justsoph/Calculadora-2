45b5154cd7147126a9bdf282243024214d68c14e
justsoph <sophiaanaklumythic@gmail.com>
2021-04-08 09:00:47 +0900

    tudo pronto

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,58 +1,44 @@
-// This is a JavaScript file
 var textoDisplay = "";
-
-//Valor 1 das operações
-var valor1 = 0;
-var valor2 = 0;
-var resultado = 0;
-
-//identificar operador do calculo
+var valor1 = "0";
 var operador = "";
 
-//função para zerar o display
 function zerarDisplay(){
   textoDisplay = "";
-  $(".display").val("");
+   $(".display").val("");
 }
 
-//Programação dos numeros
+$(document).on("click",".ce",function(){
+  zerarDisplay();
+  valor1= 0;
+})
+
 $(document).on("click",".botao",function(){
   textoDisplay += $(this).val();
   $(".display").val(textoDisplay);
 })
 
-//Programação dos operadores
-$(document).on("click", ".OP", function(){
+$(document).on("click",".op",function(){
   operador = $(this).html();
   valor1 = textoDisplay;
   zerarDisplay();
 })
 
-//Programação do Botão CE (tentando fazer sozinho)
-$(document).on("click", ".ce", function(){
+$(document).on("click","#igual",function(){
+  var valor2 = textoDisplay;
   zerarDisplay();
-  $(".display").val("0");
-  valor1 = 0;
-})  //Consegui :))))
 
-$(document).on("click","#igual", function(){
-   valor2 =  textoDisplay;
-   zerarDisplay();
+  if(operador == "+"){
+    textoDisplay = parseFloat(valor1) + parseFloat(valor2);
+  }
+  if(operador == "-"){
+    textoDisplay = parseFloat(valor1) - parseFloat(valor2);
+  }
+  if(operador == "*"){
+    textoDisplay = parseFloat(valor1) * parseFloat(valor2);
+  }
+  if(operador == "/"){
+    textoDisplay = parseFloat(valor1) / parseFloat(valor2);
+  }
 
-   if(operador == "+"){
-        textoDisplay = parseFloat(valor1) + parseFloat(valor2); 
-        }
-
-   if(operador == "-"){
-        textoDisplay = parseFloat(valor1) - parseFloat(valor2); 
-        }
-
-   if(operador == "/"){
-        textoDisplay = parseFloat(valor1) / parseFloat(valor2); 
-        }
-
-   if(operador == "*"){
-        textoDisplay = parseFloat(valor1) * parseFloat(valor2); 
-        }
-   $(".display").val(textoDisplay);
+  $(".display").val(textoDisplay);
 })
